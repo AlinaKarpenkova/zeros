e55ddbc7cb89caa1a0e283150178910bf98f4ac6
unknown <karpenkova.alina12@gmail.com>
2019-10-13 20:58:51 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-module.exports = function zeros(expression) {
-  // your solution
+function getNumberOfTwo(number) {
+  let numberOfTwo = 0
+
+  while (number % 2 === 0) {
+    numberOfTwo++;
+    number = Math.trunc(number / 2);
+  }
+
+  return numberOfTwo;
 }
+
+function getNumberOfFive(number) {
+  let numberOfFive = 0;
+
+  while (number % 5 === 0) {
+    numberOfFive++;
+    number = Math.trunc(number / 5);
+  }
+
+  return numberOfFive;
+}
+
+module.exports = function zeros(expression) {
+  let factorials = expression.split("*");
+
+  let numberOfTwo = 0,
+    numberOfFive = 0;
+
+  for (let i = 0; i < factorials.length; i++) {
+    if (factorials[i].includes("!!")) {
+      let num = factorials[i].replace(/!/g, "");
+
+      if (num % 2 === 0) {
+        for (let j = 2; j <= num; j += 2) {
+          numberOfTwo += getNumberOfTwo(j);
+          numberOfFive += getNumberOfFive(j);
+        }
+      } else {
+        for (let j = 1; j <= num; j += 2) {
+          numberOfFive += getNumberOfFive(j);
+        }
+      }
+    } else {
+      let num = factorials[i].replace(/!/g, "");
+
+      for (let j = 1; j <= num; j++) {
+        numberOfTwo += getNumberOfTwo(j);
+        numberOfFive += getNumberOfFive(j);
+      }
+    }
+  }
+
+  return (numberOfTwo < numberOfFive) ? numberOfTwo : numberOfFive;
+};
